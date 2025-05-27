28fdca3c0f2e4d969090c00693c99dbeac05e85b
bibin20TI <91010667+bibin20TI@users.noreply.github.com>
2025-05-27 14:40:59 +0700

    update javaScript sedikit lebih dirapikan

--- script/script.js
+++ script/script.js
@@ -1,26 +1,20 @@
+// Efek hover gambar profil
 const gambar = document.querySelector('#gambar');
- 
- gambar.addEventListener('mouseenter',function(){
 
-                
-              gambar.classList.toggle('gambar');
- });
+gambar.addEventListener('mouseenter', () => {
+  gambar.classList.add('gambar');
+});
 
- gambar.addEventListener('mouseleave',function(){
+gambar.addEventListener('mouseleave', () => {
+  gambar.classList.remove('gambar');
+});
 
- 	   gambar.classList.remove('gambar');
- });
+// Galeri thumbnail klik
+const container = document.querySelector('#container');
+const gede = document.querySelector('.gede');
 
- const container = document.querySelector('#container');
-
- const gede =document.querySelector('.gede');
-
- container.addEventListener('click',function(event){
-
- 	//cek apakah yang di klik class thumb
- 	if(event.target.className == 'box'){
-          gede.src =event.target.src;
-
- 	}
-
- });
+container.addEventListener('click', function (event) {
+  if (event.target.classList.contains('box')) {
+    gede.src = event.target.src;
+  }
+});
